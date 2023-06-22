ce707af6fafeb5329602ddcda100db10bc6a2300
Haben <habenkim1@gmail.com>
2023-06-22 22:47:00 +0300

    Configured Redirect

--- next.config.js
+++ next.config.js
@@ -8,4 +8,31 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/About',
+        destination: '/about',
+        permanent: true,
+      },
+      {
+        source: '/Contact',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/Live',
+        destination: '/live',
+        permanent: true,
+      },
+      {
+        source: '/Login',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
+}
+
 
